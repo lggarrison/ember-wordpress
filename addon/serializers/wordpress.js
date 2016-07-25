14d639af4247e9e26a6dc16163a6d2bf996910cd
Lacy Garrison <lggarrison@me.com>
2016-07-24 22:48:52 -0400

    Serialize Metadata

--- addon/serializers/wordpress.js
+++ addon/serializers/wordpress.js
@@ -2,39 +2,53 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
-	isNewSerializerAPI: true,
+  isNewSerializerAPI: true,
 
-	// Here we wrap the payload in a named object after the model type
-	// because this is what Ember expects { post: { datahere } }
-	normalizeSingleResponse(store, primaryModelClass, payload, id, requestType) {
-		var payloadTemp = {};
-		payloadTemp[primaryModelClass.modelName] = [payload];
+  // Here we wrap the payload in a named object after the model type
+  // because this is what Ember expects { post: { datahere } }
+  normalizeSingleResponse(store, primaryModelClass, payload, id, requestType) {
 
-		return this._super(store, primaryModelClass, payloadTemp, id, requestType);
-	},
+    let payloadTemp = {};
 
-	// Then, we can deal with our missing root element when extracting arrays from the JSON.
-	normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
-		const payloadTemp = {};
-		const rootKey = Ember.String.pluralize(primaryModelClass.modelName);
+    payloadTemp[primaryModelClass.modelName] = [payload.data];
 
-		payloadTemp[rootKey] = payload;
+    if(payload.meta) {
+      payloadTemp.meta = payload.meta;
+    }
 
-		return this._super(store, primaryModelClass, payloadTemp, id, requestType);
-	},
+    return this._super(store, primaryModelClass, payloadTemp, id, requestType);
 
-	normalize(modelClass, resourceHash, prop) {
-		// As you get bored typing `title.rendered`, here we move the `rendered` part up.
-		if (resourceHash.content && resourceHash.title.rendered) {
-			resourceHash.content = resourceHash.content.rendered;
-			resourceHash.title = resourceHash.title.rendered;
-		}
-		if (resourceHash.title && resourceHash.title.rendered) {
-			resourceHash.title = resourceHash.title.rendered;
-		}
-		if (resourceHash.excerpt && resourceHash.excerpt.rendered) {
-			resourceHash.excerpt = resourceHash.excerpt.rendered;
-		}
-		return this._super(modelClass, resourceHash, prop);
-	}
+  },
+
+  // Then, we can deal with our missing root element when extracting arrays from the JSON.
+  normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
+
+    let payloadTemp = {};
+
+    let rootKey = Ember.String.pluralize(primaryModelClass.modelName);
+
+    payloadTemp[rootKey] = payload.data;
+
+    if(payload.meta) {
+      payloadTemp.meta = payload.meta;
+    }
+
+    return this._super(store, primaryModelClass, payloadTemp, id, requestType);
+
+  },
+
+  normalize(modelClass, resourceHash, prop) {
+    // As you get bored typing `title.rendered`, here we move the `rendered` part up.
+    if (resourceHash.content && resourceHash.title.rendered) {
+      resourceHash.content = resourceHash.content.rendered;
+      resourceHash.title = resourceHash.title.rendered;
+    }
+    if (resourceHash.title && resourceHash.title.rendered) {
+      resourceHash.title = resourceHash.title.rendered;
+    }
+    if (resourceHash.excerpt && resourceHash.excerpt.rendered) {
+      resourceHash.excerpt = resourceHash.excerpt.rendered;
+    }
+    return this._super(modelClass, resourceHash, prop);
+  }
 });
